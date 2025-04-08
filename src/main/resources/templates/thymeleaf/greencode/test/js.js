9a49606900da3dacfcc5e3fd6f5a436334924c6d
dollerpeak <kobold77@empas.com>
2025-04-08 16:40:03 +0900

    javascript 진행중

---
+++ src/main/resources/templates/thymeleaf/greencode/test/js.js
@@ -0,0 +1,35 @@
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+let wrapper = document.getElementById("wrapper");
+console.log(wrapper);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+let allInputGroup = document.getElementsByClassName("inputGroup");
+console.log(allInputGroup);
+
+let inputGroup = document.getElementsByClassName("inputGroup")[0];
+console.log(inputGroup);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+let allInput = document.querySelectorAll("input");
+console.log(allInput);
+
+let input = document.querySelector("input");
+console.log(input);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+let name = document.getElementById("name");
+console.log(name);
+let password = document.getElementById("password");
+console.log(password);
+let tel = document.getElementById("tel");
+console.log(tel);
+let email = document.getElementById("email");
+console.log(email);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+console.log(inputGroup.classList);
+
+
+
